792c334b25cc793fc3e897b357264f7502014b28
Monal5031 <monalshadi16@gmail.com>
2019-03-22 02:21:43 +0530

    Complete final changes

--- src/App.js
+++ src/App.js
@@ -20,11 +20,20 @@ class App extends Component {
     })
   }
 
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  }
+
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos  deleteTodo={ this.deleteTodo } todos={this.state.todos} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
