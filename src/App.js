c51915c8a6cb05a7fbee964f847118fa8496f92b
Monal5031 <monalshadi16@gmail.com>
2019-03-22 01:43:54 +0530

    Nest components in root

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content:'Complete your website'},
+      {id: 2, content:'Play CSGO?'}
+    ]
+  }
+
+  deleteTodo = (id) => {
+    // console.log(id);
+    let todos = this.state.todos.filter(todo => {
+      return todo.id !== id ? true : false;
+    });
+    this.setState({
+      todos
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos  deleteTodo={ this.deleteTodo } todos={this.state.todos} />
       </div>
     );
   }
